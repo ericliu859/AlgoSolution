820304268d9af4bdb840e31cf6565ffcba262930
ericliu859 <ericliu859@gmail.com>
2013-04-22 11:54:47 +0800

    v0.1

--- codechef AprilCO#2013/B.cc
+++ codechef AprilCO#2013/B.cc
@@ -4,7 +4,7 @@
 #include <algorithm>
 using namespace std;
 int main(){
-	int t,n,i,j,k;
+	int t,n,i;
 	long long m,p[8];
 	cin >> t;
 	while(t--){
@@ -12,36 +12,19 @@ int main(){
 		for(i=0;i<n;i++)
 			cin >> p[i];
 		sort(p,p+n);
-		int res = 0;
-		bool flag=0;
+		bool flag = 0;
+		long long sum=0;
 		for(i=n-1;i>=0;i--){
-			m -= p[i];
-			res++;
-			if(m==0) break;
-			else if(m<0){
-				res--;
-				m+=p[i];
-				for(j=0;j<n;j++){
-					if(p[j]>m){
-						for(k=j-1;k>=0;k--){
-							if(m-p[k]>=0){
-								m-=p[k];
-								res++;
-							}
-							else{
-								flag = 1;
-								break;
-							}
-						}
-						if(flag) break;
-					}
-				}
-				if(flag) break;
+			sum += p[i];
+			if(sum>=m){
+				flag = 1;
+				break;
 			}
 		}
-		if(i==-1) cout << "-1" << endl;
-		else cout << res << endl;
+		if(!flag) cout <<"-1" << endl;
+		else cout << n-i << endl;
 		
 	}
 	return 0;
 }
+
